c7beb61f8592a7c816872ebfe61b30a1bbe25e25
divython <84345836+divython@users.noreply.github.com>
2023-01-15 18:30:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log(`hello i am divyanshu`);
+let x=6,y=5;
+let add,del,shift,unshift;
+function myRule(x,y){
+  console.log(x+y);
+  console.log(x*y);
+}
+myRule(x,y);
+let arr=[1,2,3,4,5,6,7,8,9,10];
+arr.push(11);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.shift();
+console.log(arr);
+JSON.stringify(arr);
+console.log(arr);
